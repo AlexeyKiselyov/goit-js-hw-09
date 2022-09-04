3a5b39b0af6cd3aa7d6f3f708ae83559ea6a32c9
AlexeyKiselyov <107468585+AlexeyKiselyov@users.noreply.github.com>
2022-09-04 12:28:18 +0000

    Deploying to gh-pages from @ AlexeyKiselyov/goit-js-hw-09@852750c2637d21608dfe081e21dff6ac6a7798f6 🚀

---
+++ 01-color-switcher.099e0551.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=document.querySelector("body");t.addEventListener("click",(function(){n.style.backgroundColor=r(),o=setInterval((function(){n.style.backgroundColor=r()}),1e3),t.disabled=!0})),e.addEventListener("click",(function(){clearInterval(o),t.disabled=!1}));var o=null;function r(){return"#".concat(Math.floor(16777215*Math.random()).toString(16))}}();
+//# sourceMappingURL=01-color-switcher.099e0551.js.map
