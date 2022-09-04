852750c2637d21608dfe081e21dff6ac6a7798f6
AlexeyKiselyov <aleshakiselev123@gmail.com>
2022-09-04 15:27:52 +0300

    Add 1st task without css

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const startBtn = document.querySelector('[data-start]');
 
+const stopBtn = document.querySelector('[data-stop]');
+
+const body = document.querySelector('body');
+
+startBtn.addEventListener('click', onStartBtn);
+stopBtn.addEventListener('click', onStopBtn);
+
+let interval = null;
+
+function onStartBtn() {
+  body.style.backgroundColor = getRandomHexColor();
+
+  interval = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+
+  startBtn.disabled = true;
+}
+
+function onStopBtn() {
+  clearInterval(interval);
+  startBtn.disabled = false;
+}
+
+// --------------RandomHexColor------------------
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
